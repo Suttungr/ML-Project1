cf1fd7ae01e6dd1b6361133bb3c0d11a989168f2
Alex Lacy <al2428@nau.edu>
2019-02-15 21:39:03 -0700

    Updated Interface and Header files

--- src/NN1toKmaxPredict.h
+++ src/NN1toKmaxPredict.h
@@ -1,14 +1,9 @@
-#ifndef NN1toKmaxPredict_h
-#define NN1toKmaxPredict_h
+
 
 int NN1toKmaxPredict(
-    const int n_train_observations, const int n_test_observations, const int n_features, const int max_neighbors, 
     const double *train_inputs_ptr, //n_train_observations x n_features (Matrix)
-    // const double *test_inputs_ptr, //n_test_observations x n_features(Matrix)
     const double *test_inputs_ptr, //n_test_features
     const double *train_labels_ptr,  //n_observations (vector)
+    const int n_train_observations, const int n_test_observations, const int n_features, const int max_neighbors, 
     double *test_predictions_ptr //n_test_observations x max_neighbors
 );
-
-
-#endif
--- src/interface.cpp
+++ src/interface.cpp
@@ -1,34 +1,37 @@
-#include "knn.h"
 #include <R.h>
 #include <R_ext/Rdynload.h>
+#include "NN1toKmaxPredict.h"
+
 
 // interface functions always return void
-void knn_interface(
+void NN1toKmaxPredict_interface(
     const double *train_inputs_ptr, //n_observations x n_features
-    const double *train_label_ptr,  //n_observations
-    const double *test_input_ptr,   //n_features
-    const int *n_observations_ptr,
+    const double *test_inputs_ptr, // n_test_observations x n_features(Matrix)
+    const double *train_labels_ptr,  //n_observations
+    const int *n_train_observations_ptr,
+    const int *n_test_observations_ptr,
     const int *n_features_ptr,
     const int *max_neighbors_ptr,
-    double *test_predictions_ptr    //max_neighbors
+    double *test_predictions_ptr //n_test_observations x max_neighbors
 ){
-  int status = knn(
+  int status = NN1toKmaxPredict(
     train_inputs_ptr,
-    train_label_ptr,
-    test_input_ptr,
-    *n_observations_ptr,
+    test_inputs_ptr,
+    train_labels_ptr,
+    *n_train_observations_ptr,
+    *n_test_observations_ptr,
     *n_features_ptr,
     *max_neighbors_ptr,
     test_predictions_ptr
   );
   
   if (status != 0){
-    error("non-zero exit status from knn");
+    error("non-zero exit status from NN1toKmaxPredict");
   }
 }
 
 R_CMethodDef cMethods[] = {
-  {"knn_interface", (DL_FUNC) &knn_interface, 7},
+  {"NN1toKmaxPredict_interface", (DL_FUNC) &NN1toKmaxPredict_interface, 7},
   {NULL, NULL, 0}
 };
 
